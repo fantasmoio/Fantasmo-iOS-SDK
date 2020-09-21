6fd6bf45f86382cbd6599028ecc62a3ad6ba99f2
Krishn <krishn@github.io>
2020-09-21 14:56:54 +0530

    Pod install in demo project.

---
+++ FantasmoSDKDemo/Pods/Target Support Files/Pods-FantasmoSDKDemo-FantasmoSDKDemoUITests/Pods-FantasmoSDKDemo-FantasmoSDKDemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FantasmoSDKDemo_FantasmoSDKDemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FantasmoSDKDemo_FantasmoSDKDemoUITests
+@end
---
+++ FantasmoSDKDemo/Pods/Target Support Files/Pods-FantasmoSDKDemoTests/Pods-FantasmoSDKDemoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FantasmoSDKDemoTests : NSObject
+@end
+@implementation PodsDummy_Pods_FantasmoSDKDemoTests
+@end

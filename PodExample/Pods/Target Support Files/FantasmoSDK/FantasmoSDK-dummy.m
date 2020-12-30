2d5ab78679d88c3da3ade8d9c6bfc669f1128ca2
Krishn <krishn@github.io>
2020-12-30 20:06:35 +0530

    Removed pods folder.

--- PodExample/Pods/Target Support Files/FantasmoSDK/FantasmoSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FantasmoSDK : NSObject
-@end
-@implementation PodsDummy_FantasmoSDK
-@end
--- PodExample/Pods/Target Support Files/Pods-FantasmoDemo/Pods-FantasmoDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FantasmoDemo : NSObject
-@end
-@implementation PodsDummy_Pods_FantasmoDemo
-@end

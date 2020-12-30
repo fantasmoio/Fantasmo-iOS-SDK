5930ee14e4fd324e7dc60867a9174fed7916253d
Krishn <krishn@github.io>
2020-12-30 19:56:19 +0530

    Project changes.

--- Example/Pods/Target Support Files/Pods-FantasmoSDK_Example/Pods-FantasmoSDK_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FantasmoSDK_Example : NSObject
-@end
-@implementation PodsDummy_Pods_FantasmoSDK_Example
-@end
---
+++ PodExample/Pods/Target Support Files/Pods-FantasmoDemo/Pods-FantasmoDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FantasmoDemo : NSObject
+@end
+@implementation PodsDummy_Pods_FantasmoDemo
+@end

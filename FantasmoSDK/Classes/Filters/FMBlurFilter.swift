222a8ab74cabfb1af49ce36d6d8570cb0d053a43
lucas <lucas@thestrangeagency.com>
2021-03-30 16:48:50 +0200

    stub out filter classes

---
+++ FantasmoSDK/Classes/Filters/FMBlurFilter.swift
@@ -0,0 +1,8 @@
+//
+//  FMBlurFilter.swift
+//  FantasmoSDK
+//
+//  Created by lucas kuzma on 3/30/21.
+//
+
+import Foundation
---
+++ FantasmoSDK/Classes/Filters/FMMovementFilter.swift
@@ -0,0 +1,8 @@
+//
+//  FMMovementFilter.swift
+//  FantasmoSDK
+//
+//  Created by lucas kuzma on 3/30/21.
+//
+
+import Foundation

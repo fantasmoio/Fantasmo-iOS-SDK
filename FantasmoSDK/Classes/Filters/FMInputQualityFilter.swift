04c64207cea88c8be07126506306447fd473a2b6
lucas <lucas@thestrangeagency.com>
2021-04-01 09:51:57 +0200

    track top remedies

--- FantasmoSDK/Classes/Filters/FMInputQualityFilter.swift
+++ FantasmoSDK/Classes/Filters/FMInputQualityFilter.swift
@@ -38,12 +38,20 @@ public class FMInputQualityFilter {
         FMBlurFilter(),
     ]
     
+    var remedies: [FMRemedy: Int] = [:]
+    var topRemedy: FMRemedy {
+        let topRemedy = remedies.sorted { $0.1 < $1.1 }[0].key
+        remedies.removeValue(forKey: topRemedy)
+        return topRemedy
+    }
+    
     func accepts(_ frame: ARFrame) -> Bool {
         
         // run frame through filters
         for filter in filters {
-            if filter.accepts(frame) != .accepted {
+            if case let .rejected(remedy) = filter.accepts(frame) {
                 _ = throughputAverager.addSample(value: 0.0)
+                addRemedy(remedy)
                 return false
             }
         }
@@ -51,5 +59,13 @@ public class FMInputQualityFilter {
         // success, the frame is acceptable
         _ = throughputAverager.addSample(value: 1.0)
         return true
+    }
+    
+    func addRemedy(_ remedy: FMRemedy) {
+        if remedies.keys.contains(remedy) {
+            remedies[remedy]! &+= 1
+        } else {
+            remedies[remedy] = 0
+        }
     }
 }

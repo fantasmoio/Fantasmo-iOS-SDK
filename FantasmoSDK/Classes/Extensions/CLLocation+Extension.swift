4942ea2157147756503aafdfcea6a227bc97fa99
lucas <lucas@thestrangeagency.com>
2021-08-04 10:30:36 +0200

    first pass at median

--- FantasmoSDK/Classes/Extensions/CLLocation+Extension.swift
+++ FantasmoSDK/Classes/Extensions/CLLocation+Extension.swift
@@ -21,11 +21,58 @@ extension CLLocation {
         return CLLocation(latitude: x, longitude: y)
     }
 
-    static func geometricMedian(_ locations: [CLLocation]) -> CLLocation {
-        for _ in locations {
-            // FIXME calculate median
+    static func geometricMedian(_ locations: [CLLocation], maxIterations: Int = 200) -> CLLocation {
+        // initialize guess to the centroid
+        var median = geometricMean(locations).coordinate
+
+        // if the initial guess is one of the points, the math breaks, so move it
+        while !locations.filter({
+            $0.coordinate.longitude == median.longitude &&
+            $0.coordinate.latitude == median.latitude
+        }).isEmpty {
+            median.latitude += 0.1
         }
-        return CLLocation()
+
+        var converged = false
+        var distances: [Double] = []
+        var iteration = 0
+        while !converged && iteration < maxIterations {
+            var x = 0.0
+            var y = 0.0
+            var denum = 0.0
+            var d = 0.0
+            let medianLocation = CLLocation(latitude: median.latitude, longitude: median.longitude)
+            for location in locations {
+                let distance = location.distance(from: medianLocation)
+                x += location.coordinate.latitude / distance
+                y += location.coordinate.longitude / distance
+                denum += 1.0 / distance
+                d += distance * distance
+            }
+            distances.append(d)
+
+            if denum == 0 {
+                log.error("Could not compute median!")
+                return CLLocation()
+            }
+
+            // update our guess for the median
+            median.latitude = x / denum
+            median.longitude = y / denum
+
+            // test convergence
+            if iteration > 3 {
+                converged = abs(distances[iteration] - distances[iteration-2]) < 0.1
+            }
+
+            iteration += 1
+        }
+
+        if iteration == maxIterations {
+            log.error("Median did not converge after \(maxIterations) iterations!")
+        }
+
+        return CLLocation(latitude: median.latitude, longitude: median.longitude)
     }
 
     static func medianOfAbsoluteDistances(_ locations: [CLLocation], _ median: CLLocation) -> Double {

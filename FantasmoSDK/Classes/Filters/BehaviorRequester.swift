860366e8c9744c1c1dcb5362748c54280e9ed5aa
lucas <lucas@thestrangeagency.com>
2021-08-16 08:02:59 +0200

    simplify behavior request logic

--- FantasmoSDK/Classes/Filters/BehaviorRequester.swift
+++ FantasmoSDK/Classes/Filters/BehaviorRequester.swift
@@ -2,7 +2,7 @@
 //  BehaviorRequester.swift
 //  FantasmoSDK
 //
-//  Created by Yauheni Klishevich on 15.04.2021.
+//  Created by lucas kuzma on 8/16/21.
 //
 
 import Foundation
@@ -15,48 +15,37 @@ class BehaviorRequester {
     /// The number of times the rejection must occur before triggering.
     private let incidenceThreshold = 30
 
-    private var lastTimeOfTriggering = clock()
-    private var requestHandler: ((FMBehaviorRequest) -> Void)
+    private var lastTriggerTime = clock()
     private var rejectionCounts = [FMFilterRejectionReason : Int]()
-    private var lastFrameFilterResult: FMFrameFilterResult?
+
+    private var requestHandler: ((FMBehaviorRequest) -> Void)
 
     init(handler: @escaping (FMBehaviorRequest) -> Void) {
         self.requestHandler = handler
     }
-    
+
     func processResult(_ frameFilterResult: FMFrameFilterResult) {
         switch frameFilterResult {
         case .accepted:
-            rejectionCounts.removeAll(keepingCapacity: true)
+            break
         case .rejected(let rejectionReason):
-            let count = (rejectionCounts[rejectionReason] ?? 0) &+ 1
-            
-            if count == 1, case .accepted = lastFrameFilterResult {
-                lastTimeOfTriggering = clock()
+            var count = rejectionCounts[rejectionReason] == nil ? 0 : rejectionCounts[rejectionReason]!
+            count += 1
+
+            if count > incidenceThreshold {
+                let elapsed = Double(clock() - lastTriggerTime) / Double(CLOCKS_PER_SEC)
+                if elapsed > throttleThreshold {
+                    requestHandler(rejectionReason.mapToBehaviorRequest())
+                    restart()
+                }
+            } else {
                 rejectionCounts[rejectionReason] = count
             }
-            else {
-                let elapsed = Double(clock() - lastTimeOfTriggering) / Double(CLOCKS_PER_SEC)
-                
-                if elapsed > throttleThreshold, count >= incidenceThreshold {
-                    requestHandler(rejectionReason.mapToBehaviorRequest())
-                    startNewCycle()
-                }
-                else {
-                    rejectionCounts[rejectionReason] = count
-                }
-            }
         }
-        lastFrameFilterResult = frameFilterResult
     }
     
     func restart() {
-        startNewCycle()
-    }
-    
-    private func startNewCycle() {
-        lastTimeOfTriggering = clock()
+        lastTriggerTime = clock()
         rejectionCounts.removeAll(keepingCapacity: true)
     }
-
 }

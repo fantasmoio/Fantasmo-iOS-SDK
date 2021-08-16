3c91227ed952a7417692fec74cb3b58953da3b23
lucas <lucas@thestrangeagency.com>
2021-08-16 07:03:33 +0200

    improve naming

--- FantasmoSDK/Classes/Filters/FMInputQualityFilter.swift
+++ FantasmoSDK/Classes/Filters/FMInputQualityFilter.swift
@@ -13,11 +13,10 @@ import ARKit
 /// If it is necessary to process a new sequence of frames, then `startOrRestartFiltering()` must be invoked.
 class FMInputQualityFilter: FMFrameFilter {
     
-    /// We use `clock()` rathern than `Date()` as it is likely faster. Some hint at this can be found at https://bit.ly/3vExXcZ
-    private var timestampOfLastAcceptedFrame: clock_t?
+    private var lastAcceptTime: clock_t?
     
     /// The number of seconds after which we forcibly accept a frame.
-    private var acceptanceThreshold = 3.0
+    private var acceptanceThreshold = 1.0
     
     /// Filter collection, in order of increasing computational cost
     private let filters: [FMFrameFilter] = [
@@ -27,39 +26,34 @@ class FMInputQualityFilter: FMFrameFilter {
         FMBlurFilter(),
     ]
 
-    /// Invoke this method when it is needed to start validating a new sequence of frames.
-    /// Impl details: Invoking this method will ensure that first frame on the new sequence will not be force approved without assessing for quality.
+    /// Start or restart filtering
     func startOrRestartFiltering() {
-        timestampOfLastAcceptedFrame = nil
+        lastAcceptTime = nil
     }
     
-    /// Indicate whether passed `frame` should be used for the localization.
-    /// Frame is assessed for quality by various aspects and is accepted without any assessment if the last successfully accepted
-    /// frame was passed long ago.
-    /// If it is needed to start working with new sequence of frames then invoke `startOrRestartFiltering()` or create new instance
-    /// of this class.
+    /// Accepted frames should be used for the localization.
     func accepts(_ frame: ARFrame) -> FMFrameFilterResult {
         if shouldForciblyAccept(frame) {
-            timestampOfLastAcceptedFrame = clock()
+            lastAcceptTime = clock()
             return .accepted
         }
         
         for filter in filters {
             if case let .rejected(reason) = filter.accepts(frame) {
-                if timestampOfLastAcceptedFrame == nil {
-                    timestampOfLastAcceptedFrame = clock()
+                if lastAcceptTime == nil {
+                    lastAcceptTime = clock()
                 }
                 return .rejected(reason: reason)
             }
         }
         
-        timestampOfLastAcceptedFrame = clock()
+        lastAcceptTime = clock()
         return .accepted
     }
 
     /// If there are a lot of continuous rejections, we force an acceptance
     private func shouldForciblyAccept(_ frame: ARFrame) -> Bool {
-        if let t = timestampOfLastAcceptedFrame {
+        if let t = lastAcceptTime {
             let elapsedTime = Double(clock() - t) / Double(CLOCKS_PER_SEC)
             return (elapsedTime > acceptanceThreshold)
         }
@@ -67,7 +61,6 @@ class FMInputQualityFilter: FMFrameFilter {
             return false
         }
     }
-    
 }
 
 /// Used for internal testing of filters

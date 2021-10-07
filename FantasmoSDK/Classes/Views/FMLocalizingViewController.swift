612c34e106a97176d9826d73608b0b8480446160
Nick Jensen <nickrjensen@gmail.com>
2021-10-07 11:41:36 +0200

    Revert "starts simple api"
    This reverts commit e22aaabe3187595a08ecd161d22f4e14ae4e4134.

--- FantasmoSDK/Classes/Views/FMLocalizingViewController.swift
+++ FantasmoSDK/Classes/Views/FMLocalizingViewController.swift
@@ -2,14 +2,56 @@
 //  FMLocalizingViewController.swift
 //  FantasmoSDK
 //
-//  Created by Nick Jensen on 06.10.21.
+//  Created by Nick Jensen on 29.09.21.
 //
 
-import Foundation
 import UIKit
 
-public protocol FMLocalizingViewController: UIViewController {
+public protocol FMLocalizingViewControllerProtocol: UIViewController {
     func didUpdateLocation(_ result: FMLocationResult)
     func didRequestBehavior(_ behavior: FMBehaviorRequest)
     func didFailWithError(_ error: Error, errorMetadata metadata: Any?)
+}
+
+public class FMLocalizingViewController: UIViewController {
+    var label: FMTipLabel!
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.init(white: 0.0, alpha: 0.5)
+        
+        label = FMTipLabel()
+        label.setText("Point at stores, signs and buildings around you to get a precise location")
+        view.addSubview(label)
+    }
+        
+    public override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let bounds = self.view.bounds
+        let labelMaxSize = CGSize(width: 300.0, height: bounds.height)
+        var labelRect = CGRect.zero
+        labelRect.size = label.sizeThatFits(labelMaxSize)
+        labelRect.origin.x = bounds.midX - 0.5 * labelRect.width
+        labelRect.origin.y = bounds.midY - 0.5 * labelRect.height
+        label.frame = labelRect
+    }
+}
+
+extension FMLocalizingViewController: FMLocalizingViewControllerProtocol {
+    public func didUpdateLocation(_ result: FMLocationResult) {
+        label.setText("Success")
+        view.setNeedsLayout()
+    }
+    
+    public func didRequestBehavior(_ behavior: FMBehaviorRequest) {
+        label.setText(behavior.rawValue)
+        view.setNeedsLayout()
+    }
+    
+    public func didFailWithError(_ error: Error, errorMetadata metadata: Any?) {
+        label.setText(error.localizedDescription)
+        view.setNeedsLayout()
+    }
 }
--- FantasmoSDK/Classes/Views/FMSessionViewController.swift
+++ FantasmoSDK/Classes/Views/FMSessionViewController.swift
@@ -33,22 +33,22 @@ public final class FMSessionViewController: UIViewController {
     
     private var qrCodeDetector = QRCodeDetector()
     
-    private var qrScanningViewControllerType: FMQRScanningViewController.Type = FMSimpleQRScanningViewController.self
+    private var qrScanningViewControllerType: FMQRScanningViewControllerProtocol.Type = FMQRScanningViewController.self
     
-    private var qrScanningViewController: FMQRScanningViewController? { self.children.first as? FMQRScanningViewController }
+    private var qrScanningViewController: FMQRScanningViewControllerProtocol? { self.children.first as? FMQRScanningViewControllerProtocol }
     
     /**
      Registers a custom view controller class to present and use when scanning QR codes.
-     - Parameter classType: Any class type conforming to FMQRScanningViewController.
+     - Parameter classType: Any class type conforming to FMQRScanningViewControllerProtocol.
      */
-    public func registerQRScanningViewController(_ classType: FMQRScanningViewController.Type) {
+    public func registerQRScanningViewController(_ classType: FMQRScanningViewControllerProtocol.Type) {
         qrScanningViewControllerType = classType
     }
 
     /**
      Presents the default or custom registered QR scanning view controller and starts observing QR codes in the ARSession.
      
-     Detected QR codes can be handled either via `FMSessionViewControllerDelegate` or in a registered `FMQRScanningViewController` implementation.
+     Detected QR codes can be handled either via `FMSessionViewControllerDelegate` or in a registered `FMQRScanningViewControllerProtocol` implementation.
      This method automatically stops localizing.
      */
     public func startQRScanning() {
@@ -87,15 +87,15 @@ public final class FMSessionViewController: UIViewController {
             
     private let fmLocationManagerAccessTokenInfoPlistKey = "FMLocationManagerAccessToken"
     
-    private var localizingViewControllerType: FMLocalizingViewController.Type = FMSimpleLocalizingViewController.self
+    private var localizingViewControllerType: FMLocalizingViewControllerProtocol.Type = FMLocalizingViewController.self
     
-    private var localizingViewController: FMLocalizingViewController? { self.children.first as? FMLocalizingViewController }
+    private var localizingViewController: FMLocalizingViewControllerProtocol? { self.children.first as? FMLocalizingViewControllerProtocol }
 
     /**
      Registers a custom view controller type to present and use when localizing.
-     - Parameter classType: Any class type conforming to FMLocalizingViewController.
+     - Parameter classType: Any class type conforming to FMLocalizingViewControllerProtocol.
      */
-    public func registerLocalizingViewController(_ classType: FMLocalizingViewController.Type) {
+    public func registerLocalizingViewController(_ classType: FMLocalizingViewControllerProtocol.Type) {
         localizingViewControllerType = classType
     }
 
@@ -104,7 +104,7 @@ public final class FMSessionViewController: UIViewController {
      
      - Parameter sessionId: Identifier for a unique localization session for use by analytics and billing. The max length of the string is 64 characters.
      
-     Localization results can be handled either via `FMSessionViewControllerDelegate` or in a registered `FMLocalizingViewController` implementation.
+     Localization results can be handled either via `FMSessionViewControllerDelegate` or in a registered `FMLocalizingViewControllerProtocol` implementation.
      This method automatically stops QR scanning.
      */
     public func startLocalizing(sessionId: String) {

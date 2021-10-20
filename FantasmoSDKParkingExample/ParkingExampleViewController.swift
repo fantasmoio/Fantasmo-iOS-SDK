b2c4e563f316b890ebecee4b5c86c54e2d206106
Nick Jensen <nickrjensen@gmail.com>
2021-10-20 15:48:23 +0200

    adds map result button to example app, closes #MW-320

--- FantasmoSDKParkingExample/ParkingExampleViewController.swift
+++ FantasmoSDKParkingExample/ParkingExampleViewController.swift
@@ -8,14 +8,17 @@
 import UIKit
 import FantasmoSDK
 import CoreLocation
+import MapKit
 
 class ParkingExampleViewController: UIViewController {
         
     @IBOutlet var resultLabel: UILabel!
     @IBOutlet var isSimulationSwitch: UISwitch!
     @IBOutlet var showsStatisticsSwitch: UISwitch!
+    @IBOutlet var mapPinButton: UIButton!
     
     var errorCount: Int = 0
+    var lastResult: FMLocationResult?
     
     @IBAction func handleEndRideButton(_ button: UIButton) {
         /// Test location of a parking space in Berlin
@@ -75,13 +78,16 @@ extension ParkingExampleViewController: FMParkingViewControllerDelegate {
         /// Got a localization result
         /// Localization will continue until you dismiss the view
         /// You should decide on acceptable criteria for a result, one way is by checking the `confidence` value
-        if result.confidence != .low {
-            /// We're satisfied with the result, dismiss to stop localizing
-            parkingViewController.dismiss(animated: true) {
-                let coordinate = result.location.coordinate
-                self.resultLabel.text = "Coordinates: \(coordinate.latitude), \(coordinate.longitude)\n\nConfidence: \(result.confidence)"
-                self.errorCount = 0
-            }
+        if result.confidence == .low {
+            return
+        }
+        /// We're satisfied with the result, dismiss to stop localizing
+        parkingViewController.dismiss(animated: true) {
+            let coordinate = result.location.coordinate
+            self.resultLabel.text = "Coordinates: \(coordinate.latitude), \(coordinate.longitude)\n\nConfidence: \(result.confidence)"
+            self.errorCount = 0
+            self.lastResult = result
+            self.mapPinButton.isHidden = false
         }
     }
     
@@ -97,12 +103,30 @@ extension ParkingExampleViewController: FMParkingViewControllerDelegate {
         /// Localization will continue until you dismiss the view
         /// You should decide on an acceptable threshold of errors and dismiss the view when it's reached
         errorCount += 1
-        if errorCount >= 5 {
-            /// Too many errors, dismiss to stop localizing
-            parkingViewController.dismiss(animated: true) {
-                self.resultLabel.text = error.localizedDescription
-                self.errorCount = 0
-            }
+        if errorCount < 5 {
+            return
         }
+        /// Too many errors, dismiss to stop localizing
+        parkingViewController.dismiss(animated: true) {
+            self.resultLabel.text = error.localizedDescription
+            self.errorCount = 0
+            self.lastResult = nil
+            self.mapPinButton.isHidden = true
+        }
+    }
+}
+
+extension ParkingExampleViewController {
+    /// Show the lastResult on a MKMapView
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let mapView = segue.destination.view as? MKMapView, let result = lastResult else {
+            return
+        }
+        let resultAnnotation = MKPointAnnotation()
+        resultAnnotation.coordinate = result.location.coordinate
+        let resultSpan = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
+        let resultRegion = MKCoordinateRegion(center: result.location.coordinate, span: resultSpan)
+        mapView.addAnnotation(resultAnnotation)
+        mapView.setRegion(resultRegion, animated: false)
     }
 }

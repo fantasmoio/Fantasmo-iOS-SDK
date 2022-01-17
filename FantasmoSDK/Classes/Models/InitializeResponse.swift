0988c532266b21286f96686024be37ce6d39cf6a
Nick Jensen <nickrjensen@gmail.com>
2022-01-17 15:45:57 +0100

    don't fail if remote config decoding fails

--- FantasmoSDK/Classes/Models/InitializeResponse.swift
+++ FantasmoSDK/Classes/Models/InitializeResponse.swift
@@ -10,4 +10,20 @@ import Foundation
 struct InitializeResponse: Codable {
     let parkingInRadius: Bool
     let config: RemoteConfig.Config?
+
+    enum CodingKeys: String, CodingKey {
+        case parkingInRadius
+        case config
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        parkingInRadius = try container.decode(Bool.self, forKey: .parkingInRadius)
+        do {
+            config = try container.decodeIfPresent(RemoteConfig.Config.self, forKey: .config)
+        } catch let jsonDecodeError {
+            log.error("error decoding remote config - \(jsonDecodeError.localizedDescription)")
+            config = nil
+        }
+    }
 }

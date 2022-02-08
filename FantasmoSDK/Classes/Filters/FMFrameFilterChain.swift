67167c67062715be801750d3f2b7e8a2369f64b9
Nick Jensen <nickrjensen@gmail.com>
2022-02-08 18:11:12 +0100

    adds pre + post image enhancement logic to filter chain

--- FantasmoSDK/Classes/Filters/FMFrameFilterChain.swift
+++ FantasmoSDK/Classes/Filters/FMFrameFilterChain.swift
@@ -17,8 +17,14 @@ class FMFrameFilterChain {
     /// The number of seconds after which we forcibly accept a frame, bypassing the filters
     private let acceptanceThreshold: Float
     
-    /// Active frame filters
-    let filters: [FMFrameFilter]
+    /// Active filters that are run in order before image enhancement
+    let preImageEnhancementFilters: [FMFrameFilter]
+    
+    /// Active filters that are run in order after image enhancement
+    let postImageEnhancementFilters: [FMFrameFilter]
+    
+    /// All active filters, pre + post image enhancement
+    var allFilters: [FMFrameFilter] { return preImageEnhancementFilters + postImageEnhancementFilters }
     
     /// Frame image enhancer, nil if disabled via remote config
     let imageEnhancer: FMImageEnhancer?
@@ -27,10 +33,11 @@ class FMFrameFilterChain {
     init(config: RemoteConfig.Config) {
 
         acceptanceThreshold = config.frameAcceptanceThresholdTimeout
-                
-        var enabledFilters: [FMFrameFilter] = []
+            
+        // configure pre image enhancement filters
+        var enabledPreImageEnhancementFilters: [FMFrameFilter] = []
         if config.isTrackingStateFilterEnabled {
-            enabledFilters.append(FMTrackingStateFilter())
+            enabledPreImageEnhancementFilters.append(FMTrackingStateFilter())
         }
         
         if config.isCameraPitchFilterEnabled {
@@ -38,33 +45,36 @@ class FMFrameFilterChain {
                 maxUpwardTiltDegrees: config.cameraPitchFilterMaxUpwardTilt,
                 maxDownwardTiltDegrees: config.cameraPitchFilterMaxDownwardTilt
             )
-            enabledFilters.append(cameraPitchFilter)
+            enabledPreImageEnhancementFilters.append(cameraPitchFilter)
         }
         
         if config.isMovementFilterEnabled {
             let movementFilter = FMMovementFilter(
                 threshold: config.movementFilterThreshold
             )
-            enabledFilters.append(movementFilter)
+            enabledPreImageEnhancementFilters.append(movementFilter)
         }
         
+        self.preImageEnhancementFilters = enabledPreImageEnhancementFilters
+        
+        var enabledPostImageEnhancementFilters: [FMFrameFilter] = []
         if config.isBlurFilterEnabled {
             let blurFilter = FMBlurFilter(
                 varianceThreshold: config.blurFilterVarianceThreshold,
                 suddenDropThreshold: config.blurFilterSuddenDropThreshold,
                 averageThroughputThreshold: config.blurFilterAverageThroughputThreshold
             )
-            enabledFilters.append(blurFilter)
+            enabledPostImageEnhancementFilters.append(blurFilter)
         }
         
         if config.isImageQualityFilterEnabled, #available(iOS 13, *) {
             let imageQualityFilter = FMImageQualityFilter(
                 scoreThreshold: config.imageQualityFilterScoreThreshold
             )
-            enabledFilters.append(imageQualityFilter)
+            enabledPostImageEnhancementFilters.append(imageQualityFilter)
         }
         
-        filters = enabledFilters
+        self.postImageEnhancementFilters = enabledPostImageEnhancementFilters
         
         if config.isImageEnhancerEnabled {
             imageEnhancer = FMImageEnhancer(targetBrightness: config.imageEnhancerTargetBrightness)
@@ -83,12 +93,28 @@ class FMFrameFilterChain {
         guard !Thread.isMainThread else { fatalError("evaluate called from main thread") }
         
         if shouldForceAccept() {
+            // enhance the image before force accepting
+            imageEnhancer?.enhance(frame: frame)
             lastAcceptTime = clock()
             return .accepted
         }
                 
+        // evaluate pre image enhancement filters
         var result: FMFrameFilterResult = .accepted
-        for filter in self.filters {
+        for filter in self.preImageEnhancementFilters {
+            if case let .rejected(reason) = filter.accepts(frame) {
+                // return if any rejected the frame
+                return .rejected(reason: reason)
+            }
+        }
+        
+        // enhance the image applying gamma correction
+        // this makes the `enhancedImage` and `enhancedImageGamma`
+        // frame properties available to the next filters
+        imageEnhancer?.enhance(frame: frame)
+        
+        // evaluate post image enhancement filters
+        for filter in self.postImageEnhancementFilters {
             if case let .rejected(reason) = filter.accepts(frame) {
                 result = .rejected(reason: reason)
                 break
@@ -102,7 +128,7 @@ class FMFrameFilterChain {
     }
     
     func getFilter<T:FMFrameFilter>(ofType type: T.Type) -> T? {
-        return filters.first(where: { $0 is T }) as? T
+        return allFilters.first(where: { $0 is T }) as? T
     }
     
     /// If there are a lot of continuous rejections, we force an acceptance

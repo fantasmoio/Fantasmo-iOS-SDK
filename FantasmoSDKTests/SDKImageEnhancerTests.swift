3797d3311ef2c9cc625f2db21633e5ad7b80f78b
Nick Jensen <nickrjensen@gmail.com>
2022-02-10 12:31:20 +0100

    updates enhancer tests

--- FantasmoSDKTests/SDKImageEnhancerTests.swift
+++ FantasmoSDKTests/SDKImageEnhancerTests.swift
@@ -18,21 +18,10 @@ class SDKImageEnhancerTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testImageEnhancer() throws {
+    func testImageEnhancerNighttime() throws {
         let imageEnhancer = FMImageEnhancer(targetBrightness: 0.15)!
-        
-        // test enhancement on daytime frame
-        let daytimeSession = MockARSession(videoName: "parking-daytime")
-        let daytimeFrame = daytimeSession.nextFrame()!
-        imageEnhancer.enhance(frame: daytimeFrame)
-        // check `enhancedImage` and `enhancedImage` properties were set
-        XCTAssertNotNil(daytimeFrame.enhancedImage)
-        XCTAssertNotNil(daytimeFrame.enhancedImageGamma)
-        // check no gamma correction applied, daytime images exceed target brightness
-        XCTAssertEqual(daytimeFrame.enhancedImageGamma!, 1.0)
-
-        // test enhancement on nighttime frame
         let nighttimeSession = MockARSession(videoName: "parking-nighttime")
+        
         let nighttimeFrame1 = nighttimeSession.nextFrame()!
         imageEnhancer.enhance(frame: nighttimeFrame1)
         // check `enhancedImage` and `enhancedImage` properties were set
@@ -41,11 +30,26 @@ class SDKImageEnhancerTests: XCTestCase {
         // check gamma correction was applied
         XCTAssertLessThan(nighttimeFrame1.enhancedImageGamma!, 1.0)
 
-        // test increasing target brightness
-        let nighttimeFrame2 = nighttimeSession.nextFrame()!
+        // increase target brightness
         imageEnhancer.targetBrightness = 0.25
+        
+        let nighttimeFrame2 = nighttimeSession.nextFrame()!
         imageEnhancer.enhance(frame: nighttimeFrame2)
-        // check stronger (lower) gamma correction was used
+        // check `enhancedImage` and `enhancedImage` properties were set
+        XCTAssertNotNil(nighttimeFrame2.enhancedImage)
+        XCTAssertNotNil(nighttimeFrame2.enhancedImageGamma)
+        // check stronger (lower) gamma correction was applied
         XCTAssertLessThan(nighttimeFrame2.enhancedImageGamma!, nighttimeFrame1.enhancedImageGamma!)
+    }
+    
+    func testImageEnhancerDaytime() throws {
+        let imageEnhancer = FMImageEnhancer(targetBrightness: 0.15)!
+        let daytimeSession = MockARSession(videoName: "parking-daytime")
+                
+        let daytimeFrame = daytimeSession.nextFrame()!
+        imageEnhancer.enhance(frame: daytimeFrame)
+        // check no gamma correction applied, daytime images exceed target brightness
+        XCTAssertNil(daytimeFrame.enhancedImage)
+        XCTAssertNil(daytimeFrame.enhancedImageGamma)
     }
 }
